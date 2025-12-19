2ba90239fa07eb851c65d329bcd44a1b94361461
Kauan <kauanc810@gmail.com>
2025-12-19 17:27:48 -0300

    Organização

--- dia-20-cruzamento-dados/Join.sql
+++ dia-20-cruzamento-dados/Join.sql
@@ -1,3 +1,7 @@
-SELECT * FROM clientes LEFT JOIN transacoes  ON clientes.idCliente = transacoes.idCliente
+SELECT * 
+
+FROM clientes 
+
+LEFT JOIN transacoes  ON clientes.idCliente = transacoes.idCliente
 
 LIMIT 10
